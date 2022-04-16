f7698fbd5efa75c660fd449d1eab3a7208e6c067
kiran <kiran@gmail.com>
2022-04-16 16:07:01 +0530

    updating files

---
+++ src/test/java/KGF/AppTest.java
@@ -0,0 +1,20 @@
+package KGF;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
